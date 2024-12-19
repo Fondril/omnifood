1d89d4a11c1c196b3dda570b829cfdaa48b1b341
Fondril <markojovanovic.q@yahoo.com>
2024-12-19 23:03:28 +0100

    Hero section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.{html, js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      rubik: ["Rubik", "serif"],
+    },
+    colors: {
+      primary: "#e67e22",
+      textGrey555: "#555",
+      textGrey333: "#333",
+    },
+    extend: {
+      boxShadow: {
+        inset3pxWhite: "inset 0 0 0 3px #fff",
+      },
+    },
   },
   plugins: [],
 };
