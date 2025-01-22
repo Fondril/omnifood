63dd3412de027f04d5243cb310877bad9fbb15f2
Fondril <markojovanovic.q@yahoo.com>
2025-01-22 16:47:03 +0100

    Added CTA section and Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,18 @@ module.exports = {
       textGrey333: "#333",
     },
     extend: {
+      backgroundImage: {
+        CTAImage:
+          "linear-gradient(to right bottom, rgba(235,151,78,0.35), rgba(230,125,34,0.35)), url(content/img/eating.jpg)",
+      },
       boxShadow: {
         inset3pxWhite: "inset 0 0 0 3px #fff",
         cardShadow: "0 1.5rem 3rem rgba(0, 0, 0, 0.12)",
         cardShadowHover: "0 2rem 4rem rgba(0, 0, 0, 0.09)",
+        CTAShadow: "0 2rem 4rem rgba(0, 0, 0, 0.2)",
+        inputFieldShadow: "0 1rem 2rem rgba(0, 0, 0, 0.1)",
+        focusShadow: "0 0 0 8px rgba(230, 125, 34, 0.5)",
+        formFocusShadow: "0 0 0 8px rgba(253, 242, 233, 0.5)",
       },
     },
   },
